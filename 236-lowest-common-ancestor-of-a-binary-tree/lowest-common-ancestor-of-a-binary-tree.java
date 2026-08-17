a1afcd85fe3d02e94baa1464cb4f9509c7d8577b
kumarRabinder <kumarrabinder500@gmail.com>
2026-08-17 19:09:07 +0530

    Time: 15 ms (91.06%) | Memory: 69.9 MB (61.70%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,33 +9,19 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> path1 = new ArrayList<>();
-        ArrayList<TreeNode> path2 = new ArrayList<>();
-        
-        getPath(root ,path1 , p);
-        getPath(root,path2 , q);
+        if(root == null) return root;
+        if(root == p || root == q) return root;
 
-        int i = 0;
-        while(i < path1.size() && i < path2.size() && path1.get(i).equals(path2.get(i))){
-            i++;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left == null){
+            return right;
+        }else if(right == null){
+            return left;
+        }else{
+            return root;
         }
-
-        return path1.get(i-1);
         
     }
-    private boolean getPath(TreeNode root , ArrayList<TreeNode> path , TreeNode target){
-        if(root == null) return false;
-
-        path.add(root);
-
-        if(root == target) return true;
-
-        if(getPath(root.left,path,target) || getPath(root.right , path ,target)){
-            return true;
-        }
-
-        path.remove(path.size()-1);
-        
-        return false;
-    }
-}
+}    
