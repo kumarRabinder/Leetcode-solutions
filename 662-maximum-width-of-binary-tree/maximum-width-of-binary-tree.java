6e28093ad48e6d2e2cae0e3cb5e9c18f9856460a
kumarRabinder <kumarrabinder500@gmail.com>
2026-08-22 17:03:28 +0530

    Time: 1 ms (99.70%) | Memory: 45.2 MB (49.23%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -14,63 +14,40 @@
  * }
  */
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-
-        Queue<Pair> q = new LinkedList<>();
-
-        q.offer(new Pair(root, 0));
-
-        int maxWidth = 0;
-
-        while (!q.isEmpty()) {
-
-            int size = q.size();
-
-            int first = q.peek().index;
-
-            int last = first;
-
-            for (int i = 0; i < size; i++) {
-
-                Pair p = q.poll();
-
-                last = p.index;
-
-                if (p.node.left != null) {
-
-                    q.offer(new Pair(p.node.left, 2 * p.index + 1));
-
-                }
-
-                if (p.node.right != null) {
-
-                    q.offer(new Pair(p.node.right, 2 * p.index + 2));
-
-                }
-
-            }
-
-            maxWidth = Math.max(maxWidth, last - first + 1);
-
-        }
-
-        return maxWidth;
-
-    }
-     class Pair {
-
+    public class tuple{
         TreeNode node;
-
-        int index;
-
-        Pair(TreeNode node, int index) {
-
+        int num;
+        tuple(TreeNode node , int num){
             this.node = node;
-
-            this.index = index;
-
+            this.num = num;
         }
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<tuple> q = new LinkedList<>();
+        int ans = 0;
+        q.offer(new tuple(root , 0));
+        while(!q.isEmpty()){
+            int min = q.peek().num;
+            int size= q.size();
+            int first = 0;int last =0;
+
+            for(int i=0;i<size;i++){
+                int curr_id=q.peek().num - min;
+                TreeNode curr = q.peek().node;
+                q.poll();
+                if(i == 0)first=curr_id;
+                if(i == size-1) last=curr_id;
+
+                if(curr.left != null){
+                    q.offer(new tuple(curr.left , curr_id*2+1));
+                }
+                if(curr.right != null){
+                    q.offer(new tuple(curr.right ,curr_id*2+2));
+                }
+            }
+            ans = Math.max(ans , last - first + 1);
+        }
+        return ans;
 
     }
 }
